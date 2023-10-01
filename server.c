4f1a761bad2c63d35eef8d2b6ffb50cdac24e054
William Mantly <wmantly@gmail.com>
2023-10-01 02:37:09 -0400

    Free the body

--- server.c
+++ server.c
@@ -10,13 +10,15 @@
 #include "cJSON/cJSON.h"
 
 int main(){
-	printf("crunner v8 -- September 30, 2023!\n");
+	printf("crunner v9 -- November 1, 2023!\n");
 
 	int create_socket, new_socket;
 	int buffer_size = 2048000;
 	socklen_t addrlen;
 	int port = atoi( (getenv("runnerPort") != NULL) ? getenv("runnerPort") : "15000" );
 	struct sockaddr_in address;
+
+	// Add a flag to indicate whether a client is being served
 
 	if ((create_socket = socket(AF_INET, SOCK_STREAM, 0)) > 0){
 		printf("The socket was created\n");
@@ -35,13 +37,19 @@ int main(){
 
 	// loop waiting for client to connect
 	while (1){
-		// get new memory
+
+
+        // Set the flag to indicate that a client is being served
+
 		char *buffer = malloc(buffer_size);
 		char *bufferPosition = buffer;
 		int bytes_from_socket = 0;
 		int content_length = 0;
 		int parse_passed = 1;
-		char *body = "";
+		char *body = malloc(1);  // Initialize with space for '\0'
+		body[0] = '\0';
+
+		printf("top buffer:\n%s\nbody:\n%s\n", buffer, body);
 
 		if (listen(create_socket, 10) < 0) {
 			perror("server: listen");
@@ -109,13 +117,26 @@ int main(){
 				}
 			}
 
-			if((body != NULL) && (body[0] == '\0')){
-				// parse body out of request
-				char *bodyStart = strstr(buffer,"\r\n\r\n");
-				body = &bodyStart[4];
-			}
-			
-			// cache the length of the body
+
+
+			// Reset and allocate memory for body at the beginning of each loop iteration
+		    free(body);
+		    body = malloc(content_length + 1);  // Allocate memory for body (+1 for '\0')
+		    if (body == NULL) {
+		        // Handle memory allocation error
+		        perror("Memory allocation error");
+		        exit(1);
+		    }
+
+		    // Parse body out of request
+		    char *bodyStart = strstr(buffer, "\r\n\r\n");
+		    if (bodyStart != NULL) {
+		        bodyStart += 4;
+		        strncpy(body, bodyStart, content_length);
+		        body[content_length] = '\0';  // Null-terminate the string
+		    }
+
+
 			int body_length = strlen(body);
 
 			// some debug info
@@ -139,12 +160,14 @@ int main(){
 
 			printf("should not get here...\n");
 		}
+		printf("down buffer:\n%s\nbody:\n%s\n", buffer, body);
 
 		// if there are errors in parsing, kill the socket.
 		if(parse_passed == 0){
 			printf("error in request\n");
-			close(new_socket);
+			body = "";
 			free(buffer);
+			close(new_socket);
 			continue;
 		}
 
@@ -168,6 +191,8 @@ int main(){
 		char codeToRun[strlen(code)+15];
 		strcpy(codeToRun, code);
 		strcat(codeToRun, " 2>&1|base64");
+
+		cJSON_Delete(root);
 
 		// set up POPEN
 		FILE *fp;
@@ -215,6 +240,8 @@ int main(){
 		strcat(rjson, line);
 		strcat(rjson,"\"}\0");
 		
+		free(line);
+
 		int rjsonLength = strlen(rjson)+0;
 		char rjsonLength_char[20];
 		sprintf(rjsonLength_char, "%d", rjsonLength);
@@ -230,8 +257,8 @@ int main(){
 
 		// send response to client
 		write(new_socket, rex, strlen(rex));    
-		close(new_socket);
 		free(buffer);
+		close(new_socket);
 	}
 	close(create_socket);    
 	return 0;    
